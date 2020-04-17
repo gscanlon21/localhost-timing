5a315ec40b63c0ed8152cf57f11808bd5ae892da
Graham Scanlon <gscanlon21@gmail.com>
2020-04-16 21:54:29 -0500

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+`npm install --global web-ext`
+
+`web-ext run --firefox="C:\scoop\apps\firefox\current\firefox.exe"`
+
+`Ctrl-Shift-J` to access dev console
---
+++ timer.js
@@ -0,0 +1,53 @@
+// Load existent stats with the storage API.
+var gettingStoredStats = browser.storage.local.get();
+
+gettingStoredStats.then(results => {
+    // Initialize the saved stats if not yet initialized.
+    if (!results.stats) {
+        results = {
+            currentRequest: {},
+            host: {},
+        };
+    }
+
+    browser.webRequest.onBeforeRequest.addListener((evt) => {
+        // For a top-level document, documentUrl is undefined
+        if (evt.documentUrl !== undefined) { return; }
+
+        console.debug(evt);
+
+        const url = new URL(evt.url);
+        results.currentRequest[evt.requestId] = results.currentRequest[evt.requestId] || {};
+        results.currentRequest[evt.requestId].startTime = evt.timeStamp;
+
+        // Persist the updated stats.
+        browser.storage.local.set(results);
+
+    }, {urls: ["<all_urls>"]});
+
+    browser.webRequest.onCompleted.addListener((evt) => {
+        // For a top-level document, documentUrl is undefined
+        if (evt.documentUrl !== undefined) { return; }
+        
+        console.debug(evt);
+
+        const endTime = evt.timeStamp;
+        const url = new URL(evt.url);
+        results.host[url.href] =  results.host[url.href] || {};
+        results.host[url.href].duration = results.host[url.href].duration || 0
+        results.host[url.href].duration += endTime - results.currentRequest[evt.requestId].startTime;
+
+        // Persist the updated stats.
+        browser.storage.local.set(results);
+        console.debug(results);
+
+    }, {urls: ["<all_urls>"]});
+
+
+
+    // browser.webNavigation.onCompleted.addListener(evt => {
+    //     if (evt.frameId !== 0) { return; }
+    // }, {
+    //     url: [{schemes: ["http", "https"]}]}
+    // );
+});
